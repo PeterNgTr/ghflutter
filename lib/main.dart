9e000d25f5b7881b4995e3e6e9eff42ad51d8c3c
Thanh Nguyen <peter.nguyentr@gmail.com>
2019-03-28 09:40:35 +0100

    flutter learning

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'strings.dart';
+import 'ghflutter.dart';
+
+void main() => runApp(new GHFlutterApp());
+
+class GHFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: Strings.appTitle,
+      theme: new ThemeData(primaryColor: Colors.green.shade800),
+      home: new GHFlutter(),
+    );
+  }
+}
+
+
+
+
